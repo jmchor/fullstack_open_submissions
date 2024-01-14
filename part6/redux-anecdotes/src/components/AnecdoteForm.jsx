6a82adc62bba879dc045a9b9c0a6e231da959b14
Johannes Chorzempa <jchorzempa@posteo.org>
2024-01-14 13:36:47 +0100

    ex 6.3 - 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { createAnecdote } from '../reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
+
+const AnecdoteForm = () => {
+	const dispatch = useDispatch();
+
+	const newAnecdote = (e) => {
+		e.preventDefault();
+		const content = e.target.anecdote.value;
+		e.target.anecdote.value = '';
+		dispatch(createAnecdote(content));
+	};
+	return (
+		<div>
+			<h2>create new</h2>
+			<form onSubmit={newAnecdote}>
+				<div>
+					<input name='anecdote' />
+				</div>
+				<button type='submit'>create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;
