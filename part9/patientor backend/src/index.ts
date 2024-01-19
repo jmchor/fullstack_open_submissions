b1a664ddcbb2b64fe422a646d741f43b209d7c38
Johannes Chorzempa <jchorzempa@posteo.org>
2024-01-19 07:02:54 +0100

    update code

--- part9/patientor backend/src/index.ts
+++ part9/patientor backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import patientRouter from './routes/patients';
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +11,8 @@ app.get('/api/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
+
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor backend/src/services/patientService.ts
@@ -0,0 +1,14 @@
+import patientData from '../../data/patients';
+
+const getEntries = () => {
+	return patientData;
+};
+
+const addEntry = () => {
+	return null;
+};
+
+export default {
+	getEntries,
+	addEntry,
+};
